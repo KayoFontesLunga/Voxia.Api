2d7be1dd6ab34dcd88c2fae7a4b4bcbb677bd88c
KayoFontesLunga <kayolunga84@gmail.com>
2025-11-14 02:36:31 -0300

    :sparkles: feat: entities configurations impementation

---
+++ Infrastructure/Data/Configurations/CardClickStatConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Voxia.Api.Domain.Entities;
+
+namespace VoxiasApp.Infrastructure.Data.Configurations;
+
+public class CardClickStatConfiguration : IEntityTypeConfiguration<CardClickStat>
+{
+    public void Configure(EntityTypeBuilder<CardClickStat> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.CardNome)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.Data)
+            .IsRequired();
+
+        builder.Property(x => x.IsHomeCard)
+            .IsRequired();
+
+        builder.HasOne(x => x.User)
+            .WithMany(x => x.Stats)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Infrastructure/Data/Configurations/CardHomeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Voxia.Api.Domain.Entities;
+
+namespace VoxiasApp.Infrastructure.Data.Configurations;
+
+public class CardHomeConfiguration : IEntityTypeConfiguration<CardHome>
+{
+    public void Configure(EntityTypeBuilder<CardHome> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Nome)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.ImagemUrl)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(x => x.AudioUrl)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.HasOne(x => x.Categoria)
+            .WithMany(x => x.Cards)
+            .HasForeignKey(x => x.CategoriaHomeId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
